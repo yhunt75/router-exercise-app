c5bf76e744efcaa778edb920939d008f9cf62694
Jaina Morgan <jaina.morgan@bloc.io>
2021-06-21 10:44:39 -0400

    WIP update route param checkpoint to be complete example

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,8 +1,22 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function UserProfile() {
-  const params = useParams();
-  return <p>{JSON.stringify(params)}</p>;
+  const [user, setUser] = useState({});
+
+  // Use `useParams()` and `useEffect()`
+  // Load profile data from https://jsonplaceholder.typicode.com/users/${userId}
+
+  // No need to change anything below here
+  if (user.id) {
+    return Object.entries(user).map(([key, value]) => (
+      <div key={key}>
+        <label>{key}</label>: {JSON.stringify(value)}
+        <hr />
+      </div>
+    ));
+  }
+  return "Loading...";
 }
 
 export default UserProfile;
